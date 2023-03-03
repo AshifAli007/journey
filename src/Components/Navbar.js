33c6dda99a1c799a3d384c6072858273e945cf23
AshifAli007 <theashifkhan@gmail.com>
2023-03-03 10:22:30 +0530

    setting up project

---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () =>{
+    return(
+        <div className="navbar">
+            <div className="logo">MOHD ASHIF</div>
+            <div className="links">WRITING PROJECTS</div>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import Navbar from '../Components/Navbar';
+
+
+const HomePage = () =>{
+    return(
+        <div>
+            <Navbar />
+            <div className='main'>
+                <div className='animation'>
+                    i am animation
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
