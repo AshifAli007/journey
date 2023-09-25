44b5b06415cf0cbb2d0ada3b106414651b6cbd42
Mohammad Ashif <ashif@Mohammads-MacBook-Pro.local>
2023-09-25 16:03:02 -0400

    markers removed

--- src/Pages/WizardHome.js
+++ src/Pages/WizardHome.js
@@ -16,14 +16,14 @@ const WizardHome = () => {
         start: "top top",
         end: "top -500%",
         scrub: true,
-        markers: true,
+        // markers: true,
         pin: ".wizard",
         pinSpacing: true,
         pinnedContainer: ".wizard",
       },
     });
     time
-      .to("#clouds", { left: "50%" })
+      .to("#clouds", { left: "80%" })
       .to("#backgroundRocks", { top: "10%" }, 0.1)
       .to("#backRocks", { top: "1%" }, 0.1);
     // gsap.to("#clouds", {
@@ -134,59 +134,10 @@ const WizardHome = () => {
         />
       </section>
       <div className="sec">
-        <h2>Entering Dark forest</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus
-          scelerisque eleifend donec pretium vulputate sapien nec sagittis.
-          Etiam non quam lacus suspendisse faucibus interdum. Nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Elit scelerisque
-          mauris pellentesque pulvinar pellentesque. At auctor urna nunc id.
-          Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus
-          est. Eleifend mi in nulla posuere sollicitudin aliquam. Purus in massa
-          tempor nec feugiat nisl. Sagittis id consectetur purus ut. Enim
-          facilisis gravida neque convallis a cras semper auctor neque. In est
-          ante in nibh mauris cursus. Platea dictumst quisque sagittis purus.
-          Nunc scelerisque viverra mauris in. Magna etiam tempor orci eu
-          lobortis elementum nibh tellus molestie. Platea dictumst quisque
-          sagittis purus sit. Semper auctor neque vitae tempus quam pellentesque
-          nec nam. In iaculis nunc sed augue lacus viverra. Risus quis varius
-          quam quisque id diam vel. Amet luctus venenatis lectus magna fringilla
-          urna porttitor rhoncus dolor. Purus gravida quis blandit turpis
-          cursus. Enim ut sem viverra aliquet eget sit amet tellus cras. Massa
-          vitae tortor condimentum lacinia quis. Odio facilisis mauris sit amet
-          massa vitae tortor condimentum. Turpis egestas sed tempus urna et
-          pharetra. Magna eget est lorem ipsum dolor sit amet. Purus in mollis
-          nunc sed. Fames ac turpis egestas integer eget. Dolor magna eget est
-          lorem. Maecenas sed enim ut sem viverra aliquet eget sit amet. Mi quis
-          hendrerit dolor magna. Quis varius quam quisque id diam vel quam
-          elementum pulvinar. Euismod nisi porta lorem mollis aliquam ut
-          porttitor. Consequat nisl vel pretium lectus quam id leo. Nulla
-          pellentesque dignissim enim sit. Nisl suscipit adipiscing bibendum est
-          ultricies integer quis auctor. Iaculis eu non diam phasellus
-          vestibulum lorem. A arcu cursus vitae congue mauris rhoncus. Auctor
-          neque vitae tempus quam pellentesque nec. Pulvinar pellentesque
-          habitant morbi tristique. Enim ut tellus elementum sagittis.
-          Pellentesque nec nam aliquam sem et tortor. Diam donec adipiscing
-          tristique risus nec. Parturient montes nascetur ridiculus mus.
-          Pharetra diam sit amet nisl suscipit adipiscing bibendum est. Vel
-          risus commodo viverra maecenas accumsan. Donec pretium vulputate
-          sapien nec sagittis aliquam malesuada. Tempus quam pellentesque nec
-          nam. Adipiscing diam donec adipiscing tristique risus. Eget velit
-          aliquet sagittis id consectetur purus ut. Elit eget gravida cum sociis
-          natoque penatibus et magnis. Nisi est sit amet facilisis magna etiam
-          tempor orci. Libero nunc consequat interdum varius sit amet mattis.
-          Consequat mauris nunc congue nisi vitae suscipit tellus mauris a.
-          Libero id faucibus nisl tincidunt eget nullam. Nulla pellentesque
-          dignissim enim sit amet venenatis urna cursus. Aliquam purus sit amet
-          luctus venenatis lectus. Metus dictum at tempor commodo ullamcorper a.
-          Id aliquet lectus proin nibh nisl condimentum. A diam maecenas sed
-          enim ut. Pulvinar elementum integer enim neque. Elementum facilisis
-          leo vel fringilla. Vitae proin sagittis nisl rhoncus mattis rhoncus
-          urna. Quam vulputate dignissim suspendisse in est ante in. Leo vel
-          fringilla est ullamcorper eget nulla. Quam vulputate dignissim
-          suspendisse in est ante.
-        </p>
+        <div className="aboutContainer">
+          <h2>About Me</h2>
+          <div className="aboutDetails"></div>
+        </div>
       </div>
     </div>
   );
